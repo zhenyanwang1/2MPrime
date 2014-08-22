0d8956327121adef80b941de89ac037195f1311f
Zhenyan <zhenyanwang1@gmail.com>
2014-08-22 17:14:56 +0800

    Switch to time.monotonic()

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 import time
-t1=time.time()
+t1=time.monotonic()
 import sympy
 count=len(list(sympy.sieve.primerange(1,2000000+1))) #Range it up!
 print(count)
-t2=time.time()
+t2=time.monotonic()
 print("Time:{n}s".format(n=t2-t1))
