b5320314b84003e7aad41553d79269937d4be9c0
Zhenyan <zhenyanwang1@gmail.com>
2014-08-22 17:05:38 +0800

    Comments

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 import time
 t1=time.time()
 import sympy
-count=len(list(sympy.sieve.primerange(1,2000000+1)))
+count=len(list(sympy.sieve.primerange(1,2000000+1))) #Range it up!
 print(count)
 t2=time.time()
 print("Time:{n}s".format(n=t2-t1))
